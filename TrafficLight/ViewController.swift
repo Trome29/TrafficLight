ec313ca1186a1fef032456f24a5c446bee3b9785
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-09-18 16:41:01 +0200

    Preparing the interface for work

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: Outlets
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var actionButton: UIButton!
+    
+// MARK: Computed property
+    var colorsView: [UIView] {
+        [redView, yellowView, greenView]
+    }
+    
+// MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        makeDarken(views: colorsView)
+        makeRounded(views: colorsView)
+        makeCornerRadius(to: actionButton, with: 20)
     }
 
 
 }
 
+// MARK: Extension
+private extension ViewController {
+    
+    func makeDarken(views: [UIView]) {
+        views.forEach { $0.alpha = 0.3 }
+    }
+    
+    func makeRounded(views: [UIView]) {
+        views.forEach { $0.layer.cornerRadius = 80}
+    }
+    
+    func makeCornerRadius(to button: UIView, with radius: CGFloat) {
+        button.layer.cornerRadius = radius
+    }
+}
